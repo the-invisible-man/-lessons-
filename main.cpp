0667349513e0bef2c06c9cc57fee71571de2c250
The Invisible Man <granados.carlos91@gmail.com>
2016-10-02 00:18:59 -0400

    Added new logic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "my_functions.hpp"
 
-
 // This is a comment.
 
 int main()
 {
     speak("hello world");
     cout << endl;
+
+    if (true) {
+        cout << "It's true!";
+    }
+
+
     return 0;
 }
